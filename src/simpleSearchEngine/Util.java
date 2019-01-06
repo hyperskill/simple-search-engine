5e6fc2a157856f1a9b86f1ddbe44a5d09f172a9d
Vladislav Kostylev <kostylev.v@gmail.com>
2019-01-06 15:34:09 +0300

    Refactoring

--- src/simpleSearchEngine/Util.java
+++ src/simpleSearchEngine/Util.java
@@ -1,11 +1,15 @@
 package simpleSearchEngine;
 
-import java.util.*;
-import static java.nio.file.StandardOpenOption.*;
-import java.nio.file.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Util {
+class Util {
 
   private static final String DB_PATH = "db\\";
 
@@ -14,7 +18,7 @@ public class Util {
     Path file = Paths.get(DB_PATH + filename);
         try (InputStream in = Files.newInputStream(file);
           BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
-          String line = null;
+          String line;
           while ((line = reader.readLine()) != null) {
 
             String[] words = line.trim().split(" ");
@@ -32,11 +36,8 @@ public class Util {
               default:
                 System.out.println("Person entry should contain at least first name and last name delimited with space");
           }
-
         }
       }
-
     return persons;
-
     }
 }
