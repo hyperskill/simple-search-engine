fab27c7e639fefa7e633ac826432928cb9d7f0b6
aleks <alekseyvy@gmail.com>
2019-02-12 23:45:15 +0300

    Stage #3: The user menu

--- src/simpleSearchEngine/Main.java
+++ src/simpleSearchEngine/Main.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static String[] dict = input();
+
     public static void main(String[] args) {
-        searchFunc();
+        mainMenu();
     }
 
 
@@ -15,6 +17,7 @@ public class Main {
      */
     private static String[] input(){
         Scanner inp = new Scanner(System.in);
+        System.out.print("Enter the number of people: ");
         int inpNum = inp.nextInt();
         String[] asset = new String[inpNum + 1];
         System.out.println("Enter all people: ");
@@ -30,31 +33,48 @@ public class Main {
      * subroutine that search all occurances of input String in dict
      * and prints out found results.
      */
-    private static void searchFunc(){
+    private static void searchFunc(String[] dict){
         Scanner inp = new Scanner(System.in);
-        System.out.print("Enter number of people: ");
-        String[] dict = input();
-        System.out.print("Enter number of people to search: ");
-        int num = inp.nextInt();
-        while(num > 0){
-            String search = inp.next().toLowerCase();
+        String search = inp.next().toLowerCase();
             for (String s : dict) {
                 if (s.toLowerCase().contains(search)) {
                     System.out.println(s);
                 }
             }
-            num--;
-        }
-
     }
 
     /**
-     * Test subroutine that prints out all elements of array
+     * Main menu for program
+     */
+    private static void mainMenu(){
+        Scanner inp = new Scanner(System.in);
+        while(true) {
+            System.out.print("=== Menu ===\n" +
+                    "1. Find a person\n" +
+                    "2. Print all people\n" +
+                    "0. Exit");
+            System.out.print("Enter your choice: ");
+            switch (inp.nextInt()) {
+                case 1: searchFunc(dict);
+                    break;
+                case 2: printAll(dict);
+                    break;
+                case 0: System.exit(0);
+                    break;
+                default: System.out.println("Wrong input!!!\n " +
+                        "Please input a valid choice.");
+                break;
+            }
+        }
+    }
+
+    /**
+     * subroutine for print out all elements of array
      * @param arr for print out all it's elements with index
      */
-    private static void testLoop(String[] arr){
+    private static void printAll(String[] arr){
         for (int i = 0; i < arr.length; i++){
-            System.out.println(i + ") " + arr[i]);
+            System.out.println((i + 1) + ") " + arr[i]);
         }
     }
 }
